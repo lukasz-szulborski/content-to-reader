b8761838a1ffedd9c0232086934b83e8bf154d12
Darklaki <lukasz-szulborski@o2.pl>
2024-04-23 17:42:36 +0200

    Create `ReaderFile` class and tests for it

---
+++ src/services/ReaderFileBuilder/classes/ReaderFile.test.ts
@@ -0,0 +1,123 @@
+import os from "node:os";
+import fs from "node:fs/promises";
+import { v4 as uuid } from "uuid";
+
+import { ReaderFile } from "./ReaderFile";
+
+describe("Managing binaries with ReaderFile", () => {
+  test("Non-existing temporary file should cause an error", () => {
+    const badPath = `${os.tmpdir()}/TEST_BAD_${uuid()}.epub`;
+    expect(
+      () =>
+        new ReaderFile({
+          format: "EPUB",
+          temporaryPath: badPath,
+        })
+    ).toThrow(/Fatal/);
+  });
+
+  describe("Managing valid files", () => {
+    const goodPath = `${os.tmpdir()}/TEST_GOOD_${uuid()}.epub`;
+    beforeEach(async () => {
+      await fs.writeFile(
+        goodPath,
+        "Ja mogę powiedzieć coś na ten temat, że po prostu, że ja na ten temat nic za bardzo nie mogę powiedzieć."
+      );
+    });
+
+    test("Holding valid reference to a temporary file", () => {
+      const instance = new ReaderFile({
+        format: "EPUB",
+        temporaryPath: goodPath,
+      });
+      expect(instance.temporaryPath).toEqual(goodPath);
+    });
+
+    test("Creating an instance with accessible and existing file", () => {
+      expect(
+        new ReaderFile({
+          format: "EPUB",
+          temporaryPath: goodPath,
+        })
+      ).toBeInstanceOf(ReaderFile);
+    });
+
+    test("Commiting temporary files", async () => {
+      const destinationPath = `${os.tmpdir()}/DESTINATION_FILE_${uuid()}.epub`;
+
+      const instance = new ReaderFile({
+        format: "EPUB",
+        temporaryPath: goodPath,
+      });
+
+      await instance.save(destinationPath);
+
+      const savedFileExists = await (async () => {
+        try {
+          await fs.access(destinationPath);
+          return true;
+        } catch (error) {
+          return false;
+        }
+      })();
+
+      expect(savedFileExists).toBe(true);
+
+      await fs.unlink(destinationPath);
+    });
+
+    test("Cleanup", async () => {
+      const destinationPath = `${os.tmpdir()}/DESTINATION_FILE_${uuid()}.epub`;
+
+      const instance = new ReaderFile({
+        format: "EPUB",
+        temporaryPath: goodPath,
+      });
+
+      await instance.save(destinationPath);
+
+      // After commiting the file let's clean temporary files related to this `ReaderFile`
+      await instance.cleanup();
+
+      const tmpExists = await (async () => {
+        try {
+          await fs.access(goodPath);
+          return true;
+        } catch (error) {
+          return false;
+        }
+      })()
+
+      expect(tmpExists).toBe(false);
+
+      await fs.unlink(destinationPath);
+    });
+
+    test("Cleanup should be idempotent", async () => {
+      const instance = new ReaderFile({
+        format: "EPUB",
+        temporaryPath: goodPath,
+      });
+
+      await instance.cleanup();
+      await instance.cleanup();
+
+      const tmpExists = await (async () => {
+        try {
+          await fs.access(goodPath);
+          return true;
+        } catch (error) {
+          return false;
+        }
+      })()
+
+      expect(tmpExists).toBe(false);
+    })
+
+    afterEach(async () => {
+      try {
+        await fs.unlink(goodPath);
+      } catch {}
+    });
+  });
+});
---
+++ src/services/ReaderFileBuilder/classes/ReaderFile.ts
@@ -0,0 +1,98 @@
+import { accessSync, constants } from "node:fs";
+import asyncFs from "node:fs/promises";
+
+import { FileReaderBinaryFormat } from "@types";
+
+interface ReaderFileConstructor {
+  /**
+   * Path where temporary file lives
+   */
+  readonly temporaryPath: string;
+  /**
+   * Format of the binary
+   */
+  readonly format: FileReaderBinaryFormat;
+}
+
+interface ReaderFileMethods {
+  /**
+   * Creates a safe copy of the file in a given location. Put differently, commits the temporary file.
+   *
+   * @returns Buffer of the saved file
+   */
+  save(destination: string): Promise<Buffer>;
+  /**
+   * Removes all temporary files related to this `ReaderFile` so they aren't left hanging forever.
+   *
+   * Unfortunately JavaScript doesn't implement any notion of a destructor/finalizer
+   * (GC takes care of wiping this instance), therefore this function must be called explicitly
+   * when this file is no longer in use.
+   */
+  cleanup(): Promise<void>;
+}
+
+type ReaderFileClass = ReaderFileConstructor & ReaderFileMethods;
+
+/**
+ * An object representing EPUB saved in a temporary location. It contains a path to that emphemeral file.
+ *
+ * It also allows saving related EPUB to safe non-temporary directory.
+ *
+ * **After you're done with specific reader file, call `cleanup()` function to clean binaries from `/tmp`.**
+ * */
+export class ReaderFile implements ReaderFileClass {
+  private readonly _temporaryPath: string;
+  private readonly _format: FileReaderBinaryFormat;
+
+  constructor({ format, temporaryPath }: ReaderFileConstructor) {
+    // Constructor assigns properties and check whether passed
+    // file path is valid and accessible.
+    const fileAccessible = this.isTmpAccessibleSync(temporaryPath);
+    if (!fileAccessible)
+      throw new Error(
+        `Fatal error. Node cannot access file at ${temporaryPath}. Try promoting Node's OS permissions (RWX).`
+      );
+
+    this._temporaryPath = temporaryPath;
+    this._format = format;
+  }
+
+  async save(destination: string): Promise<Buffer> {
+    await asyncFs.copyFile(
+      this._temporaryPath,
+      destination,
+      asyncFs.constants.COPYFILE_EXCL
+    );
+    const newFile = await asyncFs.readFile(destination);
+    return newFile;
+  }
+
+  
+  async cleanup(): Promise<void> {
+    try {
+      await asyncFs.unlink(this._temporaryPath);
+    } catch { }
+  }
+
+  get temporaryPath() {
+    return this._temporaryPath;
+  }
+
+  get format() {
+    return this._format;
+  }
+
+  /**
+   * Check if temporary path exists and is accessible for reading.
+   *
+   * I used synchronous API for the simplicity here. I want to instantiate and verify in a single step.
+   */
+  private isTmpAccessibleSync(path: string) {
+    try {
+      const result = accessSync(path, constants.R_OK);
+      return result === undefined;
+    } catch (error) {
+      return false;
+    }
+  }
+}
